7bcefa826593248184b0c720c0e38630ec0f1295
safarxe <tech.attribute673@passinbox.com>
2025-08-03 19:01:26 +0000

    m

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,20 +13,20 @@ int _atoi(char *s)
 
     while (s[i] != '\0')
     {
-        if (s[i] == '-')
-            sign = -sign;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            found_digit = 1;
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (found_digit)
-            break;
-        i++;
+	if (s[i] == '-')
+	    sign = -sign;
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+	    found_digit = 1;
+	    result = result * 10 + (s[i] - '0');
+	}
+	else if (found_digit)
+	    break;
+	i++;
     }
 
     if (!found_digit)
-        return (0);
+	return (0);
 
     return (sign * result);
 }
