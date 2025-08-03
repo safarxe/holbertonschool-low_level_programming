594363bee2037d83d1699f13ae14d73b0ba85482
safarxe <tech.attribute673@passinbox.com>
2025-08-03 19:48:18 +0000

    commit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,31 @@
+#include "main.h"
+
 /**
  * _strcat - Concatenates two strings
  * @dest: The destination string
  * @src: The source string to append to dest
  *
- * Return: A pointer to the resulting string dest
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte.
+ *
+ * Return: Pointer to the resulting string dest
  */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
