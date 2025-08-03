d4962ad311dd76fbdb5498b7fd6cdc24508e53ed
safarxe <tech.attribute673@passinbox.com>
2025-08-03 19:17:45 +0000

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,28 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1;
+	unsigned int result = 0;
 	int found_digit = 0;
 
 	while (s[i] != '\0')
 	{
-	if (s[i] == '-')
-		sign = -sign;
-	else if (s[i] >= '0' && s[i] <= '9')
-	{
-		found_digit = 1;
-		result = result * 10 + (s[i] - '0');
-	}
-	else if (found_digit)
-		break;
-	i++;
+		if (s[i] == '-')
+			sign = -sign;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
 	}
 
 	if (!found_digit)
-	return (0);
+		return (0);
 
-	return (sign * result);
+	if (sign == -1)
+		return (-((int)result));
+	return ((int)result);
 }
