29d967ce8709deebb1d7fc096d5c25916a22c318
safarxe <tech.attribute673@passinbox.com>
2025-08-24 17:18:51 +0000

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a given function
- * @name: name to print
- * @f: pointer to function that takes a char * and returns void
+ * print_name - prints a name using a function pointer
+ * @name: name to be printed
+ * @f: pointer to a function that takes a char * and returns void
+ *
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+    if (f)
+        f(name);
 }
